d52cd0683562a57968a503108870827faa6dc3c8
vazflavia1910 <flavia.vaz@sptech.school>
2025-10-16 17:32:44 -0300

    Alteração na modelagem e inclusão do select

--- Banco_de_dados/database-v2.sql
+++ Banco_de_dados/database-v2.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE VagasIQ;
 
+drop database VagasIQ;
+
 USE VagasIQ;
 
 CREATE TABLE condutor (
@@ -41,7 +43,7 @@ CREATE TABLE localizacao (
 	bairro VARCHAR(45) NOT NULL
 );
 
-CREATE TABLE vaga (
+CREATE TABLE vagas (
 	id_vaga INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(100) NOT NULL,
 	fk_local INT,
@@ -56,20 +58,19 @@ CREATE TABLE sensor (
 	fk_vaga INT,
 	CONSTRAINT fk_vaga 
 		FOREIGN KEY (fk_vaga)
-		REFERENCES vaga(id_vaga)
+		REFERENCES vagas(id_vaga)
 );
 
 CREATE TABLE registro (
 	id_registro INT PRIMARY KEY AUTO_INCREMENT,
+    situacao TINYINT,
 	dt_registro DATETIME,
-	situacao TINYINT,
 	fk_sensor INT,
 	CONSTRAINT fk_sensor 
 		FOREIGN KEY (fk_sensor)
 		REFERENCES sensor(id_sensor),
-	fk_usuario INT,
-	CONSTRAINT fk_usuario 
-		FOREIGN KEY (fk_usuario)
+	fk_condutor INT,
+		FOREIGN KEY (fk_condutor)
 		REFERENCES condutor(id_condutor)
 );
 
@@ -97,7 +98,7 @@ INSERT INTO localizacao (logradouro, cidade, bairro) VALUES
 	('Av. Faria Lima', 'São Paulo', 'Itaim Bibi'),
 	('Rua dos Três Irmãos', 'São Paulo', 'Vila Madalena');
     
-INSERT INTO vaga (nome, fk_local) VALUES
+INSERT INTO vagas (nome, fk_local) VALUES
 	('A1', 1),
 	('A2', 1),
 	('B3', 2),
@@ -113,7 +114,7 @@ INSERT INTO sensor (estado_sensor, fk_vaga) VALUES
 	('Ativo', 5),
 	('Ativo', 6);
     
-INSERT INTO registro (dt_registro, situacao, fk_sensor, fk_usuario) VALUES
+INSERT INTO registro (dt_registro, situacao, fk_sensor, fk_condutor) VALUES
 	('2025-10-14 08:15:00', 0, 2, 1),
 	('2025-10-14 09:00:00', 1, 4, 2),
 	('2025-10-14 07:45:00', 0, 1, 3),
@@ -124,12 +125,12 @@ INSERT INTO registro (dt_registro, situacao, fk_sensor, fk_usuario) VALUES
 SHOW TABLES;
 
 -- Disponibilidade das vagas de determinada localização
-SELECT vaga.nome AS Vaga,
+SELECT vagas.nome AS Vaga,
 	CASE
 		WHEN registro.situacao = 1 THEN 'Ocupado'
         ELSE 'Livre'
 	END AS 'Situação'
-FROM localizacao JOIN vaga ON id_localizacao = fk_local
+FROM localizacao JOIN vagas ON id_localizacao = fk_local
     JOIN sensor ON id_vaga = fk_vaga
     JOIN registro ON id_sensor = fk_sensor
 WHERE id_localizacao = 1 AND estado_sensor = 'Ativo';
@@ -148,10 +149,45 @@ FROM condutor JOIN veiculo
 	ON condutor.id_condutor = veiculo.fk_condutor;
 
 -- Todas as vagas de uma determinada rua
-SELECT vaga.nome AS vaga, 
+SELECT vagas.nome AS vaga, 
 	logradouro
-FROM vaga JOIN localizacao
+FROM vagas JOIN localizacao
     ON fk_local = id_localizacao 
     JOIN sensor ON id_vaga = fk_vaga
     JOIN registro ON id_sensor = fk_sensor
 WHERE fk_local = 1;
+
+-- 
+SELECT c.genero AS Genero,
+	c.dt_nasc AS Data_Nascimento,
+	v.modelo AS Veiculo,
+	v.ano AS Ano,
+	v.tipo AS Tipo,
+	r.dt_registro AS Data,
+	s.estado_sensor AS Estado,
+	vg.nome AS Vaga,
+	l.logradouro AS Localizacao
+FROM registro AS r
+JOIN condutor AS c
+	ON r.fk_condutor = c.id_condutor
+JOIN veiculo AS v
+	ON c.id_condutor = v.fk_condutor
+JOIN sensor AS s
+	ON r.fk_sensor = s.id_sensor
+JOIN vagas AS vg
+	ON s.fk_vaga = vg.id_vaga
+JOIN localizacao AS l
+	ON vg.fk_local = l.id_localizacao;
+    
+-- Relação do condutor e o veículo e registro com um tipo de veículo específico
+SELECT c.genero AS 'Gênero',
+	v.ano AS 'Ano do veículo',
+    v.modelo AS 'Modelo do veículo',
+    v.tipo AS 'Tipo de veículo',
+    r.situacao AS 'Situacao',
+    r.dt_registro AS 'Data do registro'
+    FROM condutor AS c JOIN veiculo AS v
+    ON v.fk_condutor = c.id_condutor
+    JOIN registro AS r 
+    ON r.fk_condutor = c.id_condutor
+    WHERE v.tipo = 'carro';
