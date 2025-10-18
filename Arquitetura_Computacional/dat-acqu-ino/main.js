83142930ac83f693413629296f0c1e38408b6280
HeloisyOM <heloisy.mota@sptech.school>
2025-10-17 22:22:07 -0300

    Comentando a API linha á linha

--- Arquitetura_Computacional/dat-acqu-ino/main.js
+++ Arquitetura_Computacional/dat-acqu-ino/main.js
@@ -1,13 +1,16 @@
 // importa os bibliotecas necessários
-const serialport = require('serialport');
-const express = require('express');
-const mysql = require('mysql2');
+const serialport = require('serialport'); // responsavel pela conexão como arduino
+const express = require('express');//responsavel pela criação do servidor web da API
+const mysql = require('mysql2'); // responsavel pela conexao com o abnco
 
 // constantes para configurações
 const SERIAL_BAUD_RATE = 9600;
+
+// Caso a porta que definimos para a API (3300)  estive sendo utilziada por outro processo, vai aparecer 'EADDRINUSE' no terminal
 const SERVIDOR_PORTA = 3300;
 
 // habilita ou desabilita a inserção de dados no banco de dados
+// caso estiver desabilitado, os dados são apenas exibidos no console, sem inserção no banco.
 const HABILITAR_OPERACAO_INSERIR = true;
 
 // função para comunicação serial
@@ -15,8 +18,7 @@ const serial = async (
     //valoresSensorAnalogico,
     valoresSensorDigital,
 ) => {
-
-    // conexão com o banco de dados MySQL
+    // conexão com o banco de dados MySQL ( configurações necessárias para a conexão)
     let poolBancoDados = mysql.createPool(
         {
             host: 'localhost',
@@ -31,7 +33,7 @@ const serial = async (
     const portas = await serialport.SerialPort.list();
     const portaArduino = portas.find((porta) => porta.vendorId == 2341 && porta.productId == 43);
     if (!portaArduino) {
-        throw new Error('O arduino não foi encontrado em nenhuma porta serial');
+        throw new Error('O arduino não foi encontrado em nenhuma porta serial'); // caso não 
     }
 
     // configura a porta serial com o baud rate especificado
@@ -48,10 +50,12 @@ const serial = async (
     });
 
     // processa os dados recebidos do Arduino
+
+    // o '\r\n' Indicar que cada leitura termina com uma quebra de linha.
     arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
         console.log(data);
-        const valores = data.split(';');
-        const sensorDigital = parseInt(valores[0]);
+        const valores = data.split(';');// delimita os valores tratados com a ;
+        const sensorDigital = parseInt(valores[0]); // transforma os valores recebidos em int
         // const sensorAnalogico = parseFloat(valores[1]);
 
         // armazena os valores dos sensores nos arrays correspondentes
@@ -85,9 +89,11 @@ const servidor = (
 ) => {
     const app = express();
 
-    // configurações de requisição e resposta
+    // configurações de requisição e resposta, esse app.use(..) permite que o navegador acesse a API de diferentes origens (CORS), que é tipo de autorização para o acesso da API pelos outros sites, no nosso caso para o index poder acessá-lo
     app.use((request, response, next) => {
+        //Aqui você está dizendo ao navegador: "qualquer site pode acessar minha API
         response.header('Access-Control-Allow-Origin', '*');
+        // aqui você define quais cabeçalhos a requisão pode enviar
         response.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
         next();
     });
@@ -111,6 +117,9 @@ const servidor = (
     // arrays para armazenar os valores dos sensores
     //const valoresSensorAnalogico = [];
     const valoresSensorDigital = [];
+
+
+    // as funções SERIAL e SERVIDOR indicam a integração do hardware físico e o software com a API
 
     // inicia a comunicação serial
     await serial(
